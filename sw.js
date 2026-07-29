49d09145bc151d03b9e6b5e25060476efcc5fc51
slzcn <zsl_58@163.com>
2026-07-29 17:30:52 +0800

    修复大厅标题上方水平接缝：延长 edgeFade 缓出渐隐

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   4. 其余同源静态(图标等): stale-while-revalidate
  * 新缓存名 → 换版自动清旧缓存。
  */
-const SW_VERSION = 'eh-sw-v69-20260729-kb-vvpin3';
+const SW_VERSION = 'eh-sw-v70-20260729-bg-seam1';
 const SHELL_CACHE = 'eh-shell-' + SW_VERSION;
 const CDN_CACHE   = 'eh-cdn-' + SW_VERSION;
 
