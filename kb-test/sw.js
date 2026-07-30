fa402e157b0cf8be7c948616702c132017acc75d
slzcn <zsl_58@163.com>
2026-07-30 10:19:05 +0800

    test(keyboard): 补齐最小页 PWA 安装资源

--- kb-test/sw.js
+++ kb-test/sw.js
@@ -1,5 +1,5 @@
-const CACHE='kb-test-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='kb-test-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
