385a690d061eee7accf8bc5f47ec6dc801ab2493
slzcn <zsl_58@163.com>
2026-07-30 10:17:10 +0800

    test(keyboard): 添加独立 PWA 最小验证页

---
+++ kb-test/sw.js
@@ -0,0 +1,5 @@
+const CACHE='kb-test-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
